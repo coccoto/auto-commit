dd9356dd7f1718f6f12252f09e888a3f7b0ab66d
coccoto <soprano.lune@gmail.com>
2025-12-07 18:56:36 +0900

    2025-12-07 18:56:36

--- autoCommit.sh
+++ autoCommit.sh
@@ -5,31 +5,24 @@ set -eu
 # autoCommit.sh {ディレクトリ名} {ブランチ名}
 #
 
-function checkout() {
-    git checkout "$1"
-}
+# 引数チェック
+if [ $# -ne 2 ]; then
+    echo "実行方法: $0 {ディレクトリ名} {ブランチ名}"
+    exit 1
+fi
 
-function commit() {
-    git add -A || exit 1
-    git commit -m "$(date +'%F %T')" || exit 1
-}
-
-function push() {
-    git push origin "$1" || exit 1
-}
-
-# 本スクリプトファイルのディレクトリをセットする
+# 対象ディレクトリのパスを取得
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-
-# 作業対象のディレクトリをセットする
 TARGET_DIR="$SCRIPT_DIR/../$1"
 
-# 作業対象のディレクトリに移動する
+# 対象ディレクトリに移動
 cd "$TARGET_DIR" || exit 1
 
-# 処理を開始する
-set +e
-checkout "$2"
-set -e
-commit
-push "$2"
+# レポジトリに変更を反映
+git switch "$2" || exit 1
+if git diff --staged --quiet; then
+    echo "全ての変更をステージングします"
+    git add -A || exit 1
+fi
+git commit -m "$(date +'%F %T')" || exit 1
+git push origin "$2" || exit 1
